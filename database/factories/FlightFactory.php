6abe32e4e07e129512b6ced3f5d9dc9b9d8c5109
galpeter94 <galpeter94@gmail.com>
2025-10-15 18:41:28 +0200

    csajom (NEJEM) kijavított mindent

---
+++ database/factories/FlightFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Flight;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flight>
+ */
+class FlightFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'date' => fake()->date(),
+            'airline_id' => Flight::all()->random()->id,
+            'limit' =>fake()->randomNumber()
+        ];
+    }
+}
